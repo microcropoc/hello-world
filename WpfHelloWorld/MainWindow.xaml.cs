44ca016ab45654616b715000949b37169d63999c
MICROCROPOCPC\Alexey <microcropoc@yandex.ru>
2017-10-25 21:23:50 +0300

    fix

--- WpfHelloWorld/MainWindow.xaml.cs
+++ WpfHelloWorld/MainWindow.xaml.cs
@@ -94,7 +94,7 @@ namespace WpfHelloWorld
             var GraphModel= new PlotModel { Title = "Kinetic Graphic"};
 
             lineKin = new LineSeries { Title = "Kinetic", MarkerType = MarkerType.Circle };
-           // lineKin.Points.Add(new DataPoint(0,0));
+            lineKin.Points.Add(new DataPoint(0,0));
 
             GraphModel.Series.Add(lineKin);
 
@@ -107,7 +107,7 @@ namespace WpfHelloWorld
             var GraphModel1 = new PlotModel { Title = "Potention Graphic" };
 
             linePot = new LineSeries { Title = "Potention", MarkerType = MarkerType.Circle };
-           // linePot.Points.Add(new DataPoint(0, 0));
+            linePot.Points.Add(new DataPoint(0, 0));
 
             GraphModel1.Series.Add(linePot);
 
